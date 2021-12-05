c1024405a58706469bbb2c89d94abcc687fce01e
Mridul Tripathi <mridulmtripathi@gmail.com>
2021-12-05 19:25:17 +0530

    Added nodemon and mongo connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+mongoose.connect('mongodb+srv://mridul:<password>@cluster0.lstng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Successfully Connected to Mongo DB");
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
